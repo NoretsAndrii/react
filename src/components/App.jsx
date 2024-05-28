82711d8c9d553471313d570dc5803b4d16b64405
NoretsAndrii <147941173+NoretsAndrii@users.noreply.github.com>
2024-05-28 15:50:56 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,96 +1,24 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+
+//===========================================//
+
 import './App.css';
-import Product from './Product';
-import SearchBar from './SearchBar/SearchBar';
-import FeedbackForm from './Form/Form';
 import Slider from './Slider/Slider';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import Shop from '../pages/Shop/Shop';
 
-const images = [
-  'https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-  'https://images.pexels.com/photos/2792157/pexels-photo-2792157.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-  'https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-  'https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-  'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
-];
-
-function App() {
-  const [index, setIndex] = useState(0);
-
-  const handleClick = type => {
-    setIndex(type === 'next' ? index + 1 : index - 1);
-  };
-
-  //=========================================================
-
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    async function fetchArticles() {
-      try {
-        // 1. Встановлюємо індикатор в true перед запитом
-        setLoading(true);
-        const response = await axios.get(
-          'https://hn.algolia.com/api/v1/search?query=react'
-        );
-        console.log(response);
-        setArticles(response.data.hits);
-      } catch (error) {
-        // Тут будемо обробляти помилку
-        setError(true);
-      } finally {
-        // 2. Встановлюємо індикатор в false після запиту
-        setLoading(false);
-      }
-    }
-
-    fetchArticles();
-  }, []);
-
+export default function App() {
   return (
     <div>
-      <div>
-        <h1>Latest articles</h1>
-        {loading && <p>Loading data, please wait...</p>}
-        {error && (
-          <p>Whoops, something went wrong! Please try reloading this page!</p>
-        )}
-        {articles.length > 0 && (
-          <ul>
-            {articles.map(({ objectID, url, title }) => (
-              <li key={objectID}>
-                <a href={url} target="_blank" rel="noreferrer noopener">
-                  {title}
-                </a>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-      <h1>Best selling</h1>
-
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        price={10.99}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={14.29}
-      />
-      <SearchBar />
-      <FeedbackForm />
-      <Slider
-        url={images[index]}
-        onClick={handleClick}
-        index={index}
-        maxIndex={images.length - 1}
-      />
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/slider">Slider</NavLink>
+        <NavLink to="/shop">Shop</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/slider" element={<Slider />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
     </div>
   );
 }
-
-export default App;
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,11 +1,28 @@
-export default function Slider({ url, onClick, index, maxIndex }) {
+import { useState } from 'react';
+import css from './Slider.module.css';
+
+const images = [
+  'https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/2792157/pexels-photo-2792157.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+];
+
+export default function Slider() {
+  const [index, setIndex] = useState(0);
+
+  const handleClick = type => {
+    setIndex(type === 'next' ? index + 1 : index - 1);
+  };
+
   return (
-    <div>
-      <button onClick={() => onClick('prev')} disabled={index === 0}>
+    <div className={css.container}>
+      <button onClick={() => handleClick('prev')} disabled={index === 0}>
         Prev
       </button>
       <img
-        src={url}
+        src={images[index]}
         alt=""
         width={600}
         height={400}
@@ -13,7 +30,10 @@ export default function Slider({ url, onClick, index, maxIndex }) {
           objectFit: 'cover',
         }}
       />
-      <button onClick={() => onClick('next')} disabled={index === maxIndex}>
+      <button
+        onClick={() => handleClick('next')}
+        disabled={index === images.length - 1}
+      >
         Next
       </button>
     </div>
